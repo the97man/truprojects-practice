3a3d87ffb0bc37868cfdf8395125f49949097909
the97man <amangurana100@gmail.com>
2026-01-29 21:38:25 +0400

    Updated UI

--- src/data/ServicesData.js
+++ src/data/ServicesData.js
@@ -1,10 +1,13 @@
 import BTech from "../assets/btech-icon.png";
 import MTech from "../assets/mtech-icon.png";
-import MiniProjects from "../assets/miniprojects.png";
-import MajorProjects from "../assets/majorprojects.png";
+import MiniProjects from "../assets/miniprojects-icon.png";
+import MajorProjects from "../assets/majorprojects-icon.png";
 import IEEE from "../assets/ieee-icon.png";
 import BulkScale from "../assets/bulk-icon.png";
-// import Paper from "../assets/";
+import Papers from "../assets/paper-icon.png";
+import Documents from "../assets/document-icon.png";
+import Internships from "../assets/internship-icon.png";
+import Trainings from "../assets/training-icon.png";
 
 export const ServicesData = [
   {
@@ -13,34 +16,67 @@ export const ServicesData = [
     alt: "B. Tech",
     title: "B. Tech",
   },
+
   {
     type: "service",
     img: MTech,
     alt: "M. Tech",
     title: "M. Tech",
   },
+
   {
     type: "service",
     img: MiniProjects,
     alt: "Mini Projects",
     title: "Mini Projects",
   },
+
   {
     type: "service",
     img: MajorProjects,
     alt: "Major Projects",
     title: "Major Projects",
   },
+
   {
     type: "service",
     img: IEEE,
     alt: "IEEE Projects",
     title: "IEEE Projects",
   },
+
   {
     type: "service",
     img: BulkScale,
     alt: "Bulk Scale",
     title: "Bulk Scale",
+  },
+
+  {
+    type: "service",
+    img: Papers,
+    alt: "Paper Writing and Publishing",
+    title: "Paper Writing and Publishing",
+  },
+
+  {
+    type: "service",
+    img: Documents,
+    alt: "Document Writing and Plagiarism",
+    title: "Document Writing and Plagiarism",
+  },
+
+  {
+    type: "service",
+    img: Internships,
+    alt: "Summer Internships",
+    title: "Summer Internships",
+  },
+
+  {
+    type: "service",
+    img: Trainings,
+    alt: "Trainings",
+    title: "Trainings",
   },
 ];
